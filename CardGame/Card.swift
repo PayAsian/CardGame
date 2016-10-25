fbf451a49fabde39c4070bcdd265d357f7bddb05
PayAsian <ptre8426@csddocs.org>
2016-10-25 13:23:22 -0600

    Changed the Card file and added constructors to the code and initialized them

---
+++ CardGame/Card.swift
@@ -0,0 +1,22 @@
+//
+//  Card.swift
+//  CardGame
+//
+//  Created by Trevino, Peyton on 10/25/16.
+//  Copyright © 2016 Trevino, Peyton. All rights reserved.
+//
+
+import UIKit
+
+class Card
+{
+    private var backImage : UIImage
+    private var isFaceUp : Bool
+    
+    
+    init()
+    {
+        backImage = UIImage()
+        isFaceUp = false
+    }
+}
---
+++ CardGame/Model/PlayingCard.swift
@@ -0,0 +1,9 @@
+//
+//  PlayingCard.swift
+//  CardGame
+//
+//  Created by Trevino, Peyton on 10/25/16.
+//  Copyright © 2016 Trevino, Peyton. All rights reserved.
+//
+
+import Foundation
