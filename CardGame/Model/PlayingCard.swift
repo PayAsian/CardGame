2bf3eda81f4dcc72832635895b3a749d0a0275e2
PayAsian <ptre8426@csddocs.org>
2016-10-25 13:42:37 -0600

    I added the playing card class

--- CardGame/Model/PlayingCard.swift
+++ CardGame/Model/PlayingCard.swift
@@ -6,4 +6,45 @@
 //  Copyright © 2016 Trevino, Peyton. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class PlaingCard : Card
+{
+    private var rank : Int
+    private var suit : String
+    private var color : UIColor
+    
+    override init()
+    {
+        rank = 0
+        suit = ""
+        color = UIColor()
+        super.init()
+    }
+    
+    func getrank() -> Int
+    {
+        return rank
+    }
+    
+    func getsuit() -> String
+    {
+        return suit
+    }
+    
+    func getcolor() -> UIColor
+    {
+        return color
+    }
+    
+    override func toString() -> String
+    {
+        let description = "This playing card ranks is: \(rank) and its suit is: \(suit). And it has a  color of: \(color) "
+        
+        return description
+        
+    }
+    
+    
+    
+}
