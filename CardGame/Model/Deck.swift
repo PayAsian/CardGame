667ba03ac3b3238f2c82a1e1da404d54bf2ecc90
PayAsian <ptre8426@csddocs.org>
2016-10-31 12:34:02 -0600

    Added Deck and PlayingCardDeck subclasses

---
+++ CardGame/Model/Deck.swift
@@ -0,0 +1,14 @@
+//
+//  Deck.swift
+//  CardGame
+//
+//  Created by Trevino, Peyton on 10/31/16.
+//  Copyright © 2016 Trevino, Peyton. All rights reserved.
+//
+
+import Foundation
+
+class Deck
+{
+    
+}
---
+++ CardGame/Model/PlayingCardDeck.swift
@@ -0,0 +1,14 @@
+//
+//  PlayingCardDeck.swift
+//  CardGame
+//
+//  Created by Trevino, Peyton on 10/31/16.
+//  Copyright © 2016 Trevino, Peyton. All rights reserved.
+//
+
+import Foundation
+
+class PlayingCardDeck : Deck
+{
+    
+}
